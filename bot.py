6843570557b9b239c043b183108a9d386d31770e
lisareina <lyzareinhardt@gmail.com>
2020-11-26 17:16:30 +0100

    event testing?

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
 from twitchbot import BaseBot, load_commands_from_directory
+from twitchbot import event_handler, Command, Event, Message, Channel
+
+
+@event_handler(Event.on_user_join)
+async def on_user_join(user: str, channel: Channel):
+    print(f'{user} joined')
+
+
 
 if __name__ == '__main__':
     load_commands_from_directory('./commands')
--- commands/commands.py
+++ commands/commands.py
@@ -1,13 +1,30 @@
 from jsonhandler import FileIO
-from twitchbot import Command
+from twitchbot import Command, DummyCommand, SubCommand, CustomCommand
+
+
 import datetime
 
-@Command('hi')
+@Command('hi', aliases=['hello', 'hey'])
 async def cmd_function(msg, *args):
-    await msg.reply(f'i was called! {msg.mention}')
+    await msg.reply(f'hiiiii {msg.mention}')
 
-@Command('time')
+@Command('time', aliases=['timezone'])
 async def cmd_function(msg, *args):
     await msg.reply(
             f'time is  a social construct but here u go, it is: {(datetime.datetime.now()).strftime("%H:%M, %A %d/%m")}... gmt+1 babyy')
+
+@Command('love')
+async def cmd_function(msg, *args):
+    await msg.reply(f'what is love {msg.mention}? do u love me? say !doyou')
+
+cmd_doyou = DummyCommand('doyou')
+
+@SubCommand(cmd_doyou, 'yes')
+async def cmd_greet_yes(msg, *args):
+    await msg.reply(f'awww thank uuuu but we should just be friendsss {msg.mention}!')
+
+
+@SubCommand(cmd_doyou, 'no')
+async def cmd_greet_no(msg, *args):
+    await msg.reply(f'omggg i h8 u!!! banning u {msg.mention}!')
 
