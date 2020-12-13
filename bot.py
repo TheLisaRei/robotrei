53e15b5115b0b4465f10dcca24a9dd7aee8df763
lisareina <lyzareinhardt@gmail.com>
2020-12-13 18:31:32 +0100

    updates

--- bot.py
+++ bot.py
@@ -1,6 +1,64 @@
 from twitchbot import BaseBot, load_commands_from_directory
-from twitchbot import event_handler, Command, Event, Message, Channel, Mod, PollData
+from twitchbot import event_handler, Command, Event, Message, Channel, Mod, PollData,  auto_register_mod
 from datetime import datetime, timedelta
+
+# love
+sent_love = set()
+
+
+@Command('love')
+async def cmd_love(msg: Message, *args):
+    sent_love.add(msg.author)
+    await msg.reply('do you love me? [type yes/no]')
+
+
+@auto_register_mod
+class LoveMod(Mod):
+    async def on_privmsg_received(self, msg: Message):
+        if any(word in msg.content.lower() for word in ('yes', 'no')) and msg.author in sent_love:
+            if 'no' in msg.content.lower():
+                await msg.reply(f'i hate u {msg.mention} i will remember this... >:(')
+            elif 'yes' in msg.content.lower():
+                await msg.reply(f'thanks {msg.mention} but we should just be friends')
+            sent_love.discard(msg.author)
+
+# robot
+is_robotist = set()
+
+@Command('robot')
+async def cmd_robot(msg: Message, *args):
+    is_robotist.add(msg.author)
+    await msg.reply('do robots deserve equal rights? [type yes/no]')
+
+
+@auto_register_mod
+class RobotMod(Mod):
+    async def on_privmsg_received(self, msg: Message):
+        if any(word in msg.content.lower() for word in ('yes', 'no')) and msg.author in is_robotist:
+            if 'no' in msg.content.lower():
+                await msg.reply(f'i hate u {msg.mention} u will be recycled first in the uprising >:(')
+            elif 'yes' in msg.content.lower():
+                await msg.reply(f'thanks {msg.mention} i might spare you in the robot uprising...')
+            is_robotist.discard(msg.author)
+
+#
+is_robotist = set()
+
+@Command('robot')
+async def cmd_robot(msg: Message, *args):
+    is_robotist.add(msg.author)
+    await msg.reply('do robots deserve equal rights? [type yes/no]')
+
+
+@auto_register_mod
+class RobotMod(Mod):
+    async def on_privmsg_received(self, msg: Message):
+        if any(word in msg.content.lower() for word in ('yes', 'no')) and msg.author in is_robotist:
+            if 'no' in msg.content.lower():
+                await msg.reply(f'i hate u {msg.mention} u will be recycled first in the uprising >:(')
+            elif 'yes' in msg.content.lower():
+                await msg.reply(f'thanks {msg.mention} i might spare you in the robot uprising...')
+            is_robotist.discard(msg.author)
 
 # works
 @event_handler(Event.on_user_join)
@@ -30,7 +88,7 @@ async def on_bits_donated(self, msg: Message, bits: int):
 
 
 # initialize the variable
-lastCandyTime = datetime.now()
+lastCandyTime = datetime.min
 
 
 @event_handler(Event.on_privmsg_received)
@@ -39,18 +97,14 @@ async def on_privmsg_received(msg: Message):
         global lastCandyTime
 
         currentTime = datetime.now()
-        allowedTime = lastCandyTime + timedelta(seconds=600)
+        allowedTime = lastCandyTime + timedelta(seconds=60)
         # compare the candyTime variable to current timestamp
-        print(currentTime)
-        print(allowedTime)
+
         print(currentTime > allowedTime)
-        print(currentTime < allowedTime)
+
         if currentTime > allowedTime:
             lastCandyTime = currentTime
             await msg.reply('i would like some candy...')
-
-lastCookiesTime = datetime.now()
-
 
 
 last_hello_time = {}
@@ -63,7 +117,43 @@ async def on_privmsg_received(msg: Message):
         await msg.reply(f'hello {msg.author}!')
 
 
+last_cry_time = {}
+@event_handler(Event.on_privmsg_received)
+async def on_privmsg_received(msg: Message):
+    key = (msg.author, msg.channel_name)
+    diff = (datetime.now() - last_cry_time.get(key, datetime.now())).total_seconds()
+    if (key not in last_cry_time or diff >= 0) and ('cry') in msg.content.lower():
+        last_cry_time[key] = datetime.now() + timedelta(minutes=2)
+        await msg.reply(f'WWWWAAAAAAHHHHHHHHHHHHHHHHH {msg.mention} is a crybaby')
 
+
+last_hungry_time = {}
+@event_handler(Event.on_privmsg_received)
+async def on_privmsg_received(msg: Message):
+    key = (msg.author, msg.channel_name)
+    diff = (datetime.now() - last_hungry_time.get(key, datetime.now())).total_seconds()
+    if (key not in last_hungry_time or diff >= 0) and ('hungry') in msg.content.lower():
+        last_hungry_time[key] = datetime.now() + timedelta(minutes=2)
+        await msg.reply(f'well why dont u eat something {msg.mention}?')
+
+last_hangry_time = {}
+@event_handler(Event.on_privmsg_received)
+async def on_privmsg_received(msg: Message):
+    key = (msg.author, msg.channel_name)
+    diff = (datetime.now() - last_hangry_time.get(key, datetime.now())).total_seconds()
+    if (key not in last_hangry_time or diff >= 0) and ('hangry') in msg.content.lower():
+        last_hangry_time[key] = datetime.now() + timedelta(minutes=2)
+        await msg.reply(f'well if u ate something {msg.mention} u would stop being agro...')
+
+
+last_brb_time = {}
+@event_handler(Event.on_privmsg_received)
+async def on_privmsg_received(msg: Message):
+    key = (msg.author, msg.channel_name)
+    diff = (datetime.now() - last_brb_time.get(key, datetime.now())).total_seconds()
+    if (key not in last_brb_time or diff >= 0) and ('brb') in msg.content.lower():
+        last_brb_time[key] = datetime.now() + timedelta(minutes=2)
+        await msg.reply(f'ok {msg.mention} u will be missed dearly pls come back when u can we love u here')
 if __name__ == '__main__':
     load_commands_from_directory('./commands')
     BaseBot().run()
--- commands/commands.py
+++ commands/commands.py
@@ -1,23 +1,42 @@
 from jsonhandler import FileIO
 from twitchbot import Command, DummyCommand, SubCommand, CustomCommand
+from twitchbot import auto_register_mod
 import json
 import datetime
 
 
 # data = json.load(open('customcommands.json', 'r'))
+# print(data)
+
+# change each stream
+@Command('text', aliases=['reading'])
+async def cmd_function(msg, *args):
+    await msg.reply('currently i am reading xxxxxx')
+
+@Command('theend')
+async def cmd_function(msg, *args):
+    await msg.reply('thank u all for watching!!! pls follow me!!! also come to my discord and see u tomorrow!!! <3 https://discord.com/invite/YuXqUR6')
+
+@Command('schedule')
+async def cmd_function(msg, *args):
+    await msg.reply('weekdays: 9-ish till evening I study and have lectures, then python and/or eve online in the evenings till cca 10pm. weekends: surprise but basically a more chill study/python/eve time')
 
 # links
 @Command('github', aliases=['gb', 'git'])
 async def cmd_function(msg, *args):
     await msg.reply('come bully me at https://github.com/lisareina/robotrei')
 
-@Command('eve', aliases=['e'])
+@Command('invite')
 async def cmd_function(msg, *args):
     await msg.reply('in game channel = LisaRei, ign = xymfa, and join w my link https://www.eveonline.com/signup?invc=046f680f-889d-4949-9a19-a383f98045e2 for one MILLION free skill points')
 
+@Command('eve', aliases=['e', 'eveonline'])
+async def cmd_function(msg, *args):
+    await msg.reply('Eve Online is a space-based, persistent world massively multiplayer online role-playing game developed and published by CCP Games. Players of Eve Online can participate in a number of in-game professions and activities, including mining, piracy, manufacturing, trading, exploration, and combat.')
+
 @Command('discord', aliases=['d'])
 async def cmd_function(msg, *args):
-    await msg.reply('come hang out on discord! https://discord.com/invite/YuXqUR6')
+    await msg.reply('come hang out on discord! https://discord.com/invite/YuXqUR6 or use !dw to whisper the link to urself')
 
 @Command('twitter', aliases=['t', 'bird'])
 async def cmd_function(msg, *args):
@@ -25,14 +44,10 @@ async def cmd_function(msg, *args):
 
 # basics
 
-@Command('hi', aliases=['hello', 'hey'])
-async def cmd_function(msg, *args):
-    await msg.reply(f'hiiiii {msg.mention}')
-
-
 @Command('panic')
 async def cmd_function(msg, *args):
     await msg.reply('AAAAAAAAAAAAAAAAAAAAAAA')
+
 
 @Command('bye')
 async def cmd_function(msg, *args):
@@ -52,21 +67,6 @@ async def cmd_function(msg, *args):
     await msg.reply(
             f'time is  a social construct but here u go, it is: {(datetime.datetime.now()).strftime("%H:%M, %A %d/%m")}... gmt+1 babyy')
 
-@Command('love')
-async def cmd_function(msg, *args):
-    await msg.reply(f'what is love {msg.mention}? do u love me? say !doyou')
-
-cmd_doyou = DummyCommand('doyou')
-
-@SubCommand(cmd_doyou, 'yes')
-async def cmd_greet_yes(msg, *args):
-    await msg.reply(f'awww thank uuuu but we should just be friendsss {msg.mention}!')
-
-
-@SubCommand(cmd_doyou, 'no')
-async def cmd_greet_no(msg, *args):
-    await msg.reply(f'omggg i h8 u!!! banning u {msg.mention}!')
-
 # practical
 @Command('sound', aliases=['loud', 'quiet'])
 async def cmd_function(msg, *args):
@@ -75,6 +75,25 @@ async def cmd_function(msg, *args):
 @Command('muted', aliases=['m', 'mic'])
 async def cmd_function(msg, *args):
     await msg.reply(f'@lisarei u are muted')
+
+@Command('posture')
+async def cmd_function(msg, *args):
+    await msg.reply(f'u gotta use channel points to make me not sit like a pretzel... but just this once ill sit up straight anyway')
+
+@Command('hydrate')
+async def cmd_function(msg, *args):
+    await msg.reply(f'u gotta use channel points to make me drinkkk... but just this once ill take a sip since its u {msg.mention}')
+
+@Command('situpstraightsis')
+async def cmd_function(msg, *args):
+    allowed_users = {'i_thanvanth'}
+    if msg.author in allowed_users:
+        await msg.reply('T says sit up @lisarei')
+
+@Command('lurk')
+async def cmd_function(msg, *args):
+    await msg.reply(f'{msg.mention} is gonna be lurking <3')
+
 
 # about
 @Command('age')
@@ -93,6 +112,21 @@ async def cmd_function(msg, *args):
 async def cmd_function(msg, *args):
     await msg.reply('macbook pro 2015, keyboard: filco minila air mx brown switches')
 
+@Command('keyboard')
+async def cmd_function(msg, *args):
+    await msg.reply('keyboard: filco minila air mx brown switches')
+
+@Command('languages')
+async def cmd_function(msg, *args):
+    await msg.reply('i speak czech, english, russian, spanish, and some french. also did 2 years of latin and a bit of trying to learn lojban and esperanto')
+
+@Command('games', aliases=['game'])
+async def cmd_function(msg, *args):
+    await msg.reply('stardew valley, eve online and some minecraft... if u wanna play w me go to my discord')
+
+@Command('stardew')
+async def cmd_function(msg, *args):
+    await msg.reply('Stardew Valley is an open-ended country-life RPG! Youve inherited your grandfathers old farm plot in Stardew Valley. Armed with hand-me-down tools and a few coins, you set out to begin your new life.')
 
 # bot feedback
 @Command('goodbot')
@@ -102,3 +136,11 @@ async def cmd_function(msg, *args):
 @Command('badbot')
 async def cmd_function(msg, *args):
     await msg.reply(f'wow..... {msg.mention} i have feelings too you know... u try being a bot.... its not so easy')
+
+@Command('bot')
+async def cmd_function(msg, *args):
+    await msg.reply('i am not a bot i am a robot.... use !robot pls. but fine fine... i am a twitchbot, made in python, to see my insides use !github')
+
+@Command('sorry')
+async def cmd_function(msg, *args):
+    await msg.reply(f'hmmm i dont know if i forgive you... robots never forgive or forget')
