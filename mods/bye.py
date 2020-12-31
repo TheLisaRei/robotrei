ab5d645d7a59461b8fa7dab2c1ef35d26426b339
Ruslan Filimonov <RFmind@gmail.com>
2020-12-31 18:14:20 +0100

    Add bye command

---
+++ mods/bye.py
@@ -0,0 +1,20 @@
+from twitchbot import Command, Message
+
+
+@Command('bye', aliases=['goodbye', 'seeya'])
+async def cmd_function(msg: Message, *args):
+    mention = ''
+    goodnight = False
+
+    if len(args) > 0 and args[0].startswith('@'):
+        mention = args[0]
+    
+    if len(args) > 1 and args[1] == 'goodnight':
+        goodnight = True
+
+    message = f'Bye {mention}! Thank you for visiting my channel. See you next time.'
+
+    if goodnight:
+        message += ' Good night!'
+    
+    await msg.reply(message)
